cc2190fe45eaeb4b8b17ed788725c96768028c30
程道光 <271105328@qq.com>
2024-03-08 12:26:36 +0800

    fix: language

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import zhCn from 'element-plus/es/locale/lang/zh-cn.mjs'
 import App from './App.vue'
 
 createApp(App).use(ElementPlus, { locale: zhCn }).mount('#app')
