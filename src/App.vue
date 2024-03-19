62d50cbef37b723693051009ddbee765df8f898f
cynic <cynic2001@gmail.com>
2024-03-20 01:29:33 +0800

    Add top count and cmc link

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@ const loading = ref(false)
 const data = ref()
 const table = ref()
 const curTable = ref()
+const top_count = ref(100)
+
 let api: { [key: string]: string }
 useApi().then((res: any) => {
   api = res
@@ -23,7 +25,11 @@ const filterHandler = (value: any, row: any, column: any) => {
 const getData = () => {
   loading.value = true
   axios
-    .get(api[curTable.value])
+    .get(api[curTable.value], {
+      params: {
+        cmc_rank: `lte.${top_count.value}`,
+      },
+    })
     .then((response: any) => {
       data.value = response.data.map((item: any) => {
         return {
@@ -45,10 +51,28 @@ const changeTable = (key: any) => {
   curTable.value = `${key}`
   getData()
 }
+
+const changeTopCount = (k: any) => {
+  top_count.value = k
+  getData()
+}
+
 </script>
 
 <template>
   <template v-if="data">
+    <el-dropdown class="count_selector">
+      <el-button type="primary">
+        {{ top_count }}<el-icon class="el-icon--right"><arrow-down /></el-icon>
+      </el-button>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item v-for="k in [100, 200, 500]" @click="changeTopCount(k)">{{
+            k
+          }}</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
     <el-dropdown class="selector">
       <el-button type="primary">
         {{ curTable }}<el-icon class="el-icon--right"><arrow-down /></el-icon>
@@ -81,6 +105,16 @@ const changeTable = (key: any) => {
             :filter-method="filterHandler"
           ></el-table-column>
           <el-table-column
+            v-else-if="`${key}` === 'cmc_link'"
+            :prop="key"
+            :label="key"
+            min-width="105"
+          >
+            <template v-slot="scope">
+              <a :href="scope.row.cmc_link" target="_blank">{{ scope.row.cmc_link }}</a>
+            </template>
+          </el-table-column>
+          <el-table-column
             v-else
             :prop="key"
             :label="key"
@@ -93,6 +127,12 @@ const changeTable = (key: any) => {
 </template>
 
 <style scoped>
+.count_selector {
+  position: fixed;
+  top: 3.5vh;
+  left: 10px;
+}
+
 .selector {
   position: fixed;
   top: 3.5vh;
